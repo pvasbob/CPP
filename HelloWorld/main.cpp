b07a91b16c9aaebb147a6cff209ee94e81e1f980
pvasbob <pvasbob@gmail.com>
2023-06-26 16:54:49 -0400

    Organizing Functions in Files  v1.0

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include "utils/greet.hpp"
+
+using  namespace std;
+
+
+
+int main() {
+    greet("Qunqun");
+
+    return 0;
+}
+
---
+++ HelloWorld/utils/greet.hpp
@@ -0,0 +1,8 @@
+#ifndef UTILS_GREET
+#define UTILS_GREET
+
+#include <string>
+
+void greet(std::string name);
+
+#endif
